72c95b561f1cf40f1a7b5bc71da8a1b38f65ceb6
林欣芳 <kingfafa8520@gmail.com>
2024-12-19 23:37:26 +0800

    Member Page
    1. Display member detail

--- src/views/Member.vue
+++ src/views/Member.vue
@@ -1,8 +1,43 @@
 <script lang="ts" setup>
+import { firebaseApp } from '@/plugins/firebase';
+import { doc, getDoc, getFirestore } from 'firebase/firestore';
+import { onMounted, ref } from 'vue';
+import { getCurrentUser } from 'vuefire';
+
+const db = getFirestore(firebaseApp);
+const userInfo = ref();
+
+const getUserInfo = async() => {
+  const user = await getCurrentUser();
+    if(user) {
+      try {
+        const userDoc = doc(db, 'UserInfo', user.uid); // 指定集合和 Document ID
+        const userSnapshot = await getDoc(userDoc);
+
+        if (userSnapshot.exists()) {
+          userInfo.value = userSnapshot.data(); // 將數據存入 userInfo
+        } else {
+          console.error('No such document!');
+        }
+      } catch(err) {
+        console.log(err);
+      }
+    }
+}
+
+onMounted(async() => {
+  await getUserInfo();
+})
 </script>
 <template>
   <div class="about">
     <h1>Member page</h1>
+    <div v-if="userInfo">
+      <p>name: {{ userInfo.name }}</p>
+      <p>email: {{ userInfo.email }}</p>
+      <p>phone: {{ userInfo.phone }}</p>
+      <p>address: {{ userInfo.address }}</p>
+    </div>
   </div>
 </template>
 
