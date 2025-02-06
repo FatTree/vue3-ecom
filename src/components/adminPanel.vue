8ef1e598d3b6853e224d3a492bb353df141d9676
FatTree <kingfafa8520@gmail.com>
2025-02-06 22:17:33 +0800

    #2 fix deploy error

--- src/components/adminPanel.vue
+++ src/components/adminPanel.vue
@@ -1,28 +1,24 @@
 <script lang="ts" setup>
-import useData from '@/composable/useData';
-import { useErrorStore } from '@/stores/errorStore';
+// import useData from '@/composable/useData';
+// import { useErrorStore } from '@/stores/errorStore';
 
 
-const {
-    addToErrorList
-} = useErrorStore();
+// const {
+//     addToErrorList
+// } = useErrorStore();
 
-const api = useData();
+// const api = useData();
 
-const {
-    callApi,
-} = api;
-
-const cc = async() => {
-    await callApi(`/api/products/ddd`);
-}
+// const cc = async() => {
+//     await api.fetchedData(`/api/products/ddd`);
+// }
 
 </script>
 
 <template>
     <div class="panel">
-        <button @click="addToErrorList('error message', 'STATUS')">add error</button>
-        <button @click="cc()">404 error</button>
+        <!-- <button @click="addToErrorList('error message', 'STATUS')">add error</button>
+        <button @click="cc()">404 error</button> -->
     </div>
 </template>
 
