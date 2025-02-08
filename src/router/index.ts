ac9c858bfb3718714425ddf727920f007e24e6af
FatTree <kingfafa8520@gmail.com>
2025-02-08 16:16:57 +0800

    #29 test gitpage

--- src/router/index.ts
+++ src/router/index.ts
@@ -8,7 +8,7 @@ import App from '@/App.vue';
 
 
 const router = createRouter({
-  history: createWebHashHistory(import.meta.env.BASE_URL),
+  history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
     {
       path: '/',
